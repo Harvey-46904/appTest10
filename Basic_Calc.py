b5dea2381fb0fc281fb381c6fd3ee479ec089728
Harvey-46904 <harveympv@hotmail.com>
2019-08-12 10:04:01 -0500

    calculadora

---
+++ Basic_Calc.py
@@ -0,0 +1,12 @@
+#Developer: Harvey Riascos
+#basic cal
+#version:1.0
+
+print(".::BASIC CAL::.")
+a=10
+b=5
+add=a+b
+print("the add is: ",add)
+def sum(a,b):
+    return a+b
+print ("the add: ",sum(5,30))
